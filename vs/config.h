01a9b3d71ad899cb3006b61a81af1dc35ccb6431
Pavel P <pavlov.pavel@gmail.com>
2025-05-05 06:48:55 +0200

    Add vs2022 project files
     + config.h for msvc

---
+++ vs/config.h
@@ -0,0 +1,34 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define if the C++ compiler supports stack-allocated variable-size arrays.
+   */
+#if defined(__clang__) || defined(__GNUC__)
+#define HAVE_DYNAMIC_ARRAY 1
+#endif
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME ""
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING ""
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME ""
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION ""
+
+
+#undef/**/PACKAGE_NAME
+#undef/**/PACKAGE_STRING
+#undef/**/PACKAGE_TARNAME
+#undef/**/PACKAGE_VERSION
+
+#include "config_lib.h"
